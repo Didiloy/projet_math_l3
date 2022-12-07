66868224c8747da0af879720c21a6af8ee14df94
Didiloy <didiloy2001@gmail.com>
2022-12-07 12:30:30 +0100

    filled method

--- GraphLinearDirectedWeight.java
+++ GraphLinearDirectedWeight.java
@@ -19,12 +19,45 @@ public class GraphLinearDirectedWeight extends GraphLinear{
     }
 
     public static GraphLinearDirected inputByAdjacencyMatrixWeight(Scanner input){
-      //TODO
-        return null;
+        int order = input.nextInt();
+        GraphLinearDirected g = new GraphLinearDirected(order);
+        byte[][] matrix = new byte[order][order];
+        for (int i = 0; i < order; i++){
+            for (int j = 0; j < order; j++){
+                matrix[i][j] = input.nextByte();
+            }
+        }
+        g.setAdjacencyMatrix(matrix);
+        g.computeAdjacencyLists();
+        g.computeWeightMatrix();
+        return g;
     }
 
     public static GraphLinearDirected inputByAdjacencyListsWeight(Scanner input){
-        //TODO
-        return null;
+        int order = input.nextInt();
+        GraphLinearDirected g = new GraphLinearDirected(order);
+
+        int[][] tableau = new int[order][];
+        for(int i = 0; i < g.order(); i++){
+            int[] tmp = new int[order];
+            int compteur = 0;
+            while(true){
+                int val = input.nextInt();
+                if(val == 0){
+                    break;
+                }
+                tmp[compteur] = val;
+                compteur++;
+            }
+            g.adjacencyList[i] = new int[compteur];
+//            System.arraycopy(tmp, 0, tableau[i], 0, tableau[i].length);
+            for(int j = 0; j < g.adjacencyList[i].length; j++){
+                g.adjacencyList[i][j] = tmp[j];
+            }
+        }
+//        g.setAdjacencyList(tableau);
+        g.computeAdjacencyMatrix();
+        g.computeWeightMatrix();
+        return g;
     }
 }
